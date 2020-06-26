2af5634d80d7f31495a79a66d874a4cb30ca2e5c
Pr00sty <carlsberg.pawel@wp.pl>
2020-06-26 21:52:00 +0200

    Add Lambda

--- Autoscaling_events/main.tf
+++ Autoscaling_events/main.tf
@@ -24,3 +24,11 @@ resource "aws_iam_role" "role_for_lambda" {
   path               = "/system/"
   assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
 }
+
+resource "aws_lambda_function" "test_lambda" {
+//  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.role_for_lambda.arn
+  handler       = "exports.test"
+  runtime = "python3.7"
+}
